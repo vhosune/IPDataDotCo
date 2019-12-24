f186b5cdf8652025b6cf695ef77eff2a06d585ed
Vincent HO-SUNE <vhosune@gmail.com>
2019-12-24 18:05:33 +0100

    Initial Commit

---
+++ IPDataDotCo/IPDataDotCo.h
@@ -0,0 +1,16 @@
+//
+// Copyright © HO-SUNE Vincent. All rights reserved.
+//
+
+
+@import Foundation;
+
+//! Project version number for IPDataDotCo.
+FOUNDATION_EXPORT double IPDataDotCoVersionNumber;
+
+//! Project version string for IPDataDotCo.
+FOUNDATION_EXPORT const unsigned char IPDataDotCoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IPDataDotCo/PublicHeader.h>
+
+
